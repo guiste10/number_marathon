31320fbb9b29cec7ad025540e817d0aa4a03f1ed
Guillaume Neirinckx <Guillaume.Neirinckx@arhs-technology.com>
2022-11-27 19:49:03 +0100

    replace span with input that increases according to size

--- src/app/number-marathon/number-marathon-test-phase/cell-group/cell-group.component.ts
+++ src/app/number-marathon/number-marathon-test-phase/cell-group/cell-group.component.ts
@@ -1,17 +1,29 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cell-group',
   templateUrl: './cell-group.component.html',
   styleUrls: ['./cell-group.component.css']
 })
-export class CellGroupComponent implements OnInit{
+export class CellGroupComponent implements OnInit, AfterViewInit{
 
   @Input() size: number;
+  @ViewChild('input') input: ElementRef;
+
   cellGroup: string;
 
   ngOnInit(): void {
     this.cellGroup = Array.from({length: this.size}, () => Math.floor(Math.random() * 10)).join('');
+  }
+
+  ngAfterViewInit(): void {
+    const input = this.input.nativeElement;
+    input.style.width = input.value.length + 1 + 'ch';
+    if(input){
+      input.addEventListener('input', function() {
+        this.style.width = this.value.length + 1 + 'ch';
+      })
+    }
   }
 
 }
--- src/app/number-marathon/number-marathon-test-phase/number-marathon-test-phase.component.ts
+++ src/app/number-marathon/number-marathon-test-phase/number-marathon-test-phase.component.ts
@@ -1,18 +1,15 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { CellGroupComponent } from './cell-group/cell-group.component';
 
 @Component({
   selector: 'app-number-marathon-test-phase',
   templateUrl: './number-marathon-test-phase.component.html',
   styleUrls: ['./number-marathon-test-phase.component.css']
 })
-export class NumberMarathonTestPhaseComponent implements OnInit{
+export class NumberMarathonTestPhaseComponent {
 
   @Input() cellGroupWidth: number;
   cellGroups: Array<number>;
-
-  ngOnInit(): void {
-    //this.cellGroups = this.numSequence(3);
-  }
 
   numSequence(n: number): Array<number> {
     return Array(n);
