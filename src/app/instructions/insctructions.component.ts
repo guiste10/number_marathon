385fd5757a0fb7650af2980a7c975c1d18947063
Guillaume Neirinckx <Guillaume.Neirinckx@arhs-technology.com>
2023-05-11 00:29:26 +0200

    add missing component

---
+++ src/app/instructions/insctructions.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-insctructions',
+  templateUrl: './insctructions.component.html',
+  styleUrls: ['./insctructions.component.css']
+})
+export class InstructionsComponent {
+
+}
